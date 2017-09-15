66928983e50ea844c74f71970faca40030882da0
Fabian Venturini Cabau <fabiancabau@hotmail.com>
2017-09-15 15:47:58 -0300

    versao bonita

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+let _ = require('underscore');
 app.use('/modules', express.static(__dirname + '/node_modules/'));
+app.use('/assets', express.static(__dirname + '/assets/'));
 server.listen(3000);
 
 
@@ -12,9 +14,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/bonito', function (req, res) {
+  res.sendFile(__dirname + '/bonito.html');
+});
+
 // variavel que guarda as mensagens enviadas pelos usuarios
 // com a estrutura {from: string, msg: string}
 let messages = {};
+let privateMessages = [];
 let users = {};
 
 // função para gerenciar os eventos recebidos e enviados pelo socket
@@ -62,48 +69,84 @@ io.on('connection', function (socket) {
 
   socket.on('join-room', (data) => {
     // Sai da sala atual
-    socket.leave(socket.room);
+    if (socket.room != null && socket.room != undefined) {
+      socket.leave(socket.room);
+    }
 
     console.log(`Usuario ${socket.nome} saindo da sala ${socket.room}`);
 
     // Remove socket da array de usuarios daquela sala
-    users[socket.room] = users[socket.room].filter((v) => {
-      return v.socket_id !== socket.id
-    });
+    if (socket.room != null && socket.room != undefined) {
+      users[socket.room] = users[socket.room].filter((v) => {
+        return v.socket_id !== socket.id
+      });
+    }
 
     // Emite um evento para atualizar as informacoes de sala dos outros usuarios
     io.in(socket.room).emit('client-user-disconnected', {users: users[socket.room]});
 
-    // Entra na nova sala
-    socket.join(data.id);
-    socket.room = data.id;
+    socket.room = null;
 
-    console.log(`Usuario ${socket.nome} entrou na sala ${socket.room}`);
+    if (!data.private) {
+      // Entra na nova sala
+      socket.join(data.id);
+      socket.room = data.id;
 
-    // Cria lista de mensagens/salas se nao estiverem criadas
-    if (messages[socket.room] === undefined) {
-      messages[socket.room] = [];
+      console.log(`Usuario ${socket.nome} entrou na sala ${socket.room}`);
+
+      // Cria lista de mensagens/salas se nao estiverem criadas
+      if (messages[socket.room] === undefined) {
+        messages[socket.room] = [];
+      }
+
+      if (users[socket.room] === undefined) {
+        users[socket.room] = [];
+      }
+
+      // Adiciona usuario na nova sala
+      users[socket.room].push({nome: socket.nome, socket_id: socket.id})
+      // Emite evento com informacoes de mensagens e usuarios da nova sala
+      io.in(socket.room).emit('client-join-room', {messages: messages[socket.room], users: users[socket.room]});
+    } else {
+
+      // Procura a conversa onde tem os dois usuarios
+      let conversation = _.find(privateMessages, (v) => {
+        return (_.contains(v.users, data.id) && _.contains(v.users, socket.id));
+      });
+
+      // Se a conversa não foi encontrada, temos que criar uma nova
+      if (conversation === undefined) {
+        let conversation_id = data.id + '_' + socket.id;
+        conversation = {users: [data.id, socket.id], messages: [], id: conversation_id};
+        privateMessages.push(conversation);
+      }
+
+      // Emite o objeto de conversa
+      socket.emit('client-join-private-room', {messages: conversation.messages, id: conversation.id})
     }
 
-    if (users[socket.room] === undefined) {
-      users[socket.room] = [];
-    }
+  });
 
-    // Adiciona usuario na nova sala
-    users[socket.room].push({nome: socket.nome, socket_id: socket.id})
-    // Emite evento com informacoes de mensagens e usuarios da nova sala
-    io.in(socket.room).emit('client-join-room', {messages: messages[socket.room], users: users[socket.room]});
+  socket.on('private-message', function (data) {
+    let conversation = _.find(privateMessages, (v) => {
+      return (_.contains(v.users, data.to) && _.contains(v.users, socket.id));
+    });
+
+    conversation.messages.push({from: socket.nome, msg: data.msg});
+
+    socket.to(data.to).emit('client-private-message', conversation);
   });
 
   // Evento disparado quando usuário desconecta do socket (cai internet, fecha aba)
   socket.on('disconnect', function () {
+    if (socket.room !== undefined && socket.room !== null) {
+      // Remove socket da array de usuarios daquela sala
+      users[socket.room] = users[socket.room].filter((v) => {
+        return v.socket_id !== socket.id
+      });
 
-    // Remove socket da array de usuarios daquela sala
-    users[socket.room] = users[socket.room].filter((v) => {
-      return v.socket_id !== socket.id
-    });
-
-    // Emite um evento para atualizar as informacoes de sala dos outros usuarios
-    io.in(socket.room).emit('client-user-disconnected', {users: users[socket.room]});
+      // Emite um evento para atualizar as informacoes de sala dos outros usuarios
+      io.in(socket.room).emit('client-user-disconnected', {users: users[socket.room]});
+    }
   });
 });
